d9f2d96c84decd617144c489662c38a18d15572b
miguimono <miguimono@gmail.com>
2023-02-09 03:12:08 -0500

    Test

--- src/services/login/login.service.spec.ts
+++ src/services/login/login.service.spec.ts
@@ -1,34 +1,48 @@
-import { TestBed } from "@angular/core/testing";
-import { LoginService } from "./login.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { getTestBed, TestBed } from '@angular/core/testing';
+import { LoginService } from './login.service';
+import { ILogin, IResLogin } from 'src/models/login/login.interface';
+import { MockLogin, MockResSuccessLogin } from 'src/test/login/login.mock';
+import { environment } from 'src/environments/environment';
 
-xdescribe("LoginService", () => {
+describe('FastLoginService', () => {
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
   let service: LoginService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LoginService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LoginService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    });
+
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+    service = TestBed.get(LoginService);
   });
 
-  describe("method1", () => {
-    it("should ...", () => {
-      expect(service).toBeTruthy();
-    });
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('LoginService', () => {
+    const transfer: ILogin = MockLogin;
+    service
+      .login(transfer)
+      .subscribe((data) => expect(data).toBeTruthy());
+
+    const url = environment.api.login;
+    const req = httpMock.expectOne(url);
+    expect(req.request.method).toBe('POST');
+    req.flush({});
   });
 });
-
-/* tslint:disable:no-unused-variable */
-
-// import { TestBed, async, inject } from "@angular/core/testing";
-// import { LoginService } from "./login.service";
-
-// xdescribe("Service: Login", () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [LoginService],
-//     });
-//   });
-
-//   it("should ...", inject([LoginService], (service: LoginService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
