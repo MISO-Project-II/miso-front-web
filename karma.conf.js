36e9d6bb9c2db3f17515acbe7d2c4755997f2cf6
ChristianLoza <Christianlzap@gmail.com>
2023-02-11 21:21:37 -0500

    fixed: arguments headless

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
-      //require("karma-chrome-launcher"),
+      require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage"),
       require("@angular-devkit/build-angular/plugins/karma"),
@@ -33,8 +33,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    //browsers: ["Chrome"],
+    autoWatch: true,
+    browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true,
   });
